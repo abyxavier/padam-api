76bd531fcb022a6eaaddf061321cd31ae37fdeb0
aby <aby.xavier1@gmail.com>
2026-03-29 18:44:15 +0530

    refactor: remove root router and update CORS policy to allow all origins

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from src.presentation.api.controllers.health_controller import health_router, root_router
+from src.presentation.api.controllers.health_controller import health_router
 from src.presentation.api.controllers.recommend_controller import recommend_router
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 app.include_router(health_router)
-app.include_router(root_router)
 app.include_router(recommend_router)
 
 if __name__ == "__main__":
